e34ef8cc96de245486ac3b42b9a46d7a0d0f8dc3
Sally Fisher <sqfisher@gmail.com>
2015-07-27 00:22:26 -0500

    Added js to remove dependency on column-selector.js

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ $('td').click(function () {
   $(this).on("mouseout", checkWin);
 });
 
-
-
-function checkColor(color, col, row, diag1, diag2) {
+function checkColor(color, col, row, diag) {
   var rowCounter = 0;
   var colCounter = 0;
   var diagCount1 = 0;
@@ -18,10 +16,10 @@ function checkColor(color, col, row, diag1, diag2) {
       if (row[j].className == color) {
         colCounter++;
       }  
-      if (diag1[j].className == color) {
+      if (diag[0][j].className == color) {
         diagCount1++;
       }
-      if (diag2[j].className == color) {
+      if (diag[1][j].className == color) {
         diagCount2++;
       } 
     };
@@ -31,21 +29,34 @@ function checkColor(color, col, row, diag1, diag2) {
       return false;
     }
 }
-function checkWin() {
+
+function groupingByThree (array) {
+  var newArray = [];
+  for(var n = 0; n < 3; n++){
+    var x = $.map(array, function(val, i){
+      return (i - n) % 3 === 0 ? val : null;
+    });
+    newArray.push(x);
+   };
+ return newArray
+};
+
+
+function checkWin() {  
   var $columns = $("tbody td");
   var $rows = $("tbody tr");
-  var diag1 = [$columns[0], $columns[4], $columns[8]];
-  var diag2 = [$columns[2], $columns[4], $columns[6]];
-  for(var i = 1; i < 4; i++) {
-    var $col = $columns.nthCol(i).toArray();
-    var $row = $rows[i-1].children;
+  var diag = [ [$columns[0], $columns[4], $columns[8]], [$columns[2], $columns[4], $columns[6]] ];
+  $columns = groupingByThree($columns);
+  for(var i = 0; i < 3; i++) {
+    var $col = $columns[i];
+    var $row = $rows[i].children;
     
-    if(checkColor("red", $col, $row, diag1, diag2)) {
+    if(checkColor("red", $col, $row, diag)) {
       $("#result").text("Otter Wins!");
       return;
     }
     
-    if(checkColor("blue", $col, $row, diag1, diag2)) {
+    if(checkColor("blue", $col, $row, diag)) {
       $("#result").text("Pug Wins!");
       return;
     }
